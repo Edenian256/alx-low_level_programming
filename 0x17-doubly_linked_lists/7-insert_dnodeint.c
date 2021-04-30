1f6fea6039c35aad4dd6cd50086020df9319d6f7
Edenian256 <77830218+Edenian256@users.noreply.github.com>
2021-04-30 17:58:54 +0300

    Create 7-insert_dnodeint.c
    Insert

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert_dnodeint_at_index
+ * @h: list type dlistint_t
+ * @idx: index
+ * @n: number to insert in new node
+ * Return: head whit new node at idx position or Null
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int counter = 0;
+	dlistint_t *current_node = *h;
+	dlistint_t *new_node = NULL;
+	dlistint_t *prev_node = NULL;
+
+	if (h == NULL || (*h == NULL && idx != 0))
+		return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	while (current_node != NULL)
+	{
+		if (counter == idx)
+		{
+			new_node->next = current_node;
+			new_node->prev = current_node->prev;
+			prev_node = current_node->prev;
+			prev_node->next = new_node;
+			return (new_node);
+		}
+		current_node = current_node->next;
+		counter++;
+	}
+	if (counter == idx)
+	{
+		add_dnodeint_end(h, n);
+		return (new_node);
+	}
+
+	free(new_node);
+	return (NULL);
+}
